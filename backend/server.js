9d4b2b93018840c5c16d3fbe9cb00c0b146e79c5
Atharv Singh <atharvsingh1396@gmail.com>
2022-05-05 15:44:26 +0530

    created connection between server and client

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const { Server } = require("socket.io");
 
+var players = {}
+
 const io = new Server(8080, {
     cors: {
         origin:["http://localhost:3000"]
@@ -7,5 +9,13 @@ const io = new Server(8080, {
 })
 
 io.on('connection', (socket) => {
-    console.log(socket.id);
+    console.log("New Player Id: ",socket.id);
+
+    players[socket.id] = {
+        id: socket.id,
+        x: 100,
+        y: 100
+    }
+
+    socket.emit("players data",players);
 });
--- frontend/src/scenes/MainScene.ts
+++ frontend/src/scenes/MainScene.ts
@@ -1,15 +1,17 @@
 import Coin from "../classes/Coin";
 import Player from "../classes/Player";
-
+import {io,Socket} from "socket.io-client";
 export default class MainScene extends Phaser.Scene {
 	private player!: Player;
 	private coin!: Coin;
+	private socket!: Socket
 	constructor() {
 		super("MainScene");
 	}
 	create() {
 		this.intiObjects();
 		this.initColliders();
+		this.initSocket();
 	}
 
 	update(time: number, delta: number): void {
@@ -30,5 +32,13 @@ export default class MainScene extends Phaser.Scene {
 				Math.random() * this.game.canvas.height
 			);
 		});
+	}
+
+	initSocket(): void{
+		this.socket = io('http://localhost:8080');
+
+		this.socket.on("players data", (players) => {
+			console.log(players);
+		})
 	}
 }
