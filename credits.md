3c21596c53a9708394978a9bc5a1dfae65535683
Atharv Singh <atharvsingh1396@gmail.com>
2022-05-06 14:39:29 +0530

    added assets

---
+++ credits.md
@@ -0,0 +1,2 @@
+TileSet(JawBreaker) by https://adamatomic.itch.io/jawbreaker 
+TP-Shooter-Assets by https://klinkos.itch.io/tp-shooter-assets
--- frontend/src/scenes/LoadingScene.ts
+++ frontend/src/scenes/LoadingScene.ts
@@ -2,6 +2,11 @@ export default class LoadingScene extends Phaser.Scene{
     constructor() {
         super('LoadingScene');
     }
+
+    preload() {
+        this.load.setPath("/frames")
+        this.load.image('ilde', "._big_demon_anim_f0.png");
+    }
     create(){
         this.scene.start("MainScene");
     }
